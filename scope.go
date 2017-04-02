f45f5cc54eebfb2731c788d8320e950e5715f0b2
Ruben Nijveld <ruben@gewooniets.nl>
2017-04-02 22:00:23 +0200

    Add scope creation functions

--- scope.go
+++ scope.go
@@ -7,6 +7,51 @@ type Scope struct {
 	Network, Receiver, Sender *string
 }
 
+// NewUniversalScope returns a universal scope
+func NewUniversalScope() Scope {
+	return Scope{
+		Network:  nil,
+		Receiver: nil,
+		Sender:   nil,
+	}
+}
+
+// NewNetworkScope returns a scope limited to a network
+func NewNetworkScope(network string) Scope {
+	return Scope{
+		Network:  &network,
+		Receiver: nil,
+		Sender:   nil,
+	}
+}
+
+// NewReceiverScope returns a scope limited to a network and a specific receiver (channel) in that network
+func NewReceiverScope(network string, receiver string) Scope {
+	return Scope{
+		Network:  &network,
+		Receiver: &receiver,
+		Sender:   nil,
+	}
+}
+
+// NewSenderScope returns a scope limited to a network and a specific sender (user) in that network
+func NewSenderScope(network string, sender string) Scope {
+	return Scope{
+		Network:  &network,
+		Receiver: nil,
+		Sender:   &sender,
+	}
+}
+
+// NewScope returns a scope limited on a specific sender and receiver combination in a specific network
+func NewScope(network string, receiver string, sender string) Scope {
+	return Scope{
+		Network:  &network,
+		Receiver: &receiver,
+		Sender:   &sender,
+	}
+}
+
 // IsAll indicates if this scope is global
 func (scope Scope) IsAll() bool {
 	return scope.Network == nil && scope.Receiver == nil && scope.Sender == nil
